7f6f4521e042acc010e4689d1d3aa6f748b39583
GaussFormula <c594794@hotmail.com>
2020-07-06 23:28:11 +0800

    Build front face of box.

--- DX12SampleProgram/GeometryGenerator.cpp
+++ DX12SampleProgram/GeometryGenerator.cpp
@@ -6,5 +6,18 @@ using namespace DirectX;
 
 GeometryGenerator::MeshData GeometryGenerator::CreateBox(float width, float height, float depth, uint32 numSubdivisions)
 {
+    MeshData meshData;
 
+    // Create the verticies.
+    Vertex v[24];
+
+    float w2 = 0.5f * width;
+    float h2 = 0.5f * height;
+    float d2 = 0.5f * depth;
+
+    // Fill in the front face vertex data.
+    v[0] = Vertex(-w2, -h2, -d2, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+    v[1] = Vertex(-w2, +h2, -d2, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+    v[2] = Vertex(+w2, +h2, -d2, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+    v[3] = Vertex(+w2, -h2, -d2, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
 }
