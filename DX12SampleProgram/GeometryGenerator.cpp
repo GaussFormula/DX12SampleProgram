239d37c6e4afc17ac7abad5f4bf6cfb3341d4855
GaussFormula <c594794@hotmail.com>
2020-07-06 18:02:31 +0800

    Updated.

---
+++ DX12SampleProgram/GeometryGenerator.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "GeometryGenerator.h"
+
+using namespace DirectX;
+
+
+GeometryGenerator::MeshData GeometryGenerator::CreateBox(float width, float height, float depth, uint32 numSubdivisions)
+{
+
+}
