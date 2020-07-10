5b932e482f20711b6eac9e39f3abe7c990a14dc2
GaussFormula <c594794@hotmail.com>
2020-07-10 18:36:22 +0800

    Updated.

--- DX12SampleProgram/GeometryGenerator.cpp
+++ DX12SampleProgram/GeometryGenerator.cpp
@@ -287,3 +287,56 @@ void GeometryGenerator::Subdivide(MeshData& meshData)
         meshData.Indices32.push_back(i * 6 + 2);
     }
 }
+
+GeometryGenerator::MeshData GeometryGenerator::CreateGeosphere(float radius, uint32 numSubdivisions)
+{
+    MeshData meshData;
+
+    // Put a cap on the number of subdivisions.
+
+    // Approximate a sphere by tessellating an icosahedron.
+    const float X = 0.525731f;
+    const float Z = 0.850651f;
+
+    XMFLOAT3 pos[12] =
+    {
+        XMFLOAT3(-X, 0.0f, Z),  XMFLOAT3(X, 0.0f, Z),
+        XMFLOAT3(-X, 0.0f, -Z), XMFLOAT3(X, 0.0f, -Z),
+        XMFLOAT3(0.0f, Z, X),   XMFLOAT3(0.0f, Z, -X),
+        XMFLOAT3(0.0f, -Z, X),  XMFLOAT3(0.0f, -Z, -X),
+        XMFLOAT3(Z, X, 0.0f),   XMFLOAT3(-Z, X, 0.0f),
+        XMFLOAT3(Z, -X, 0.0f),  XMFLOAT3(-Z, -X, 0.0f)
+    };
+
+    uint32 k[60] =
+    {
+        1,4,0,  4,9,0,  4,5,9,  8,5,4,  1,8,4,
+        1,10,8, 10,3,8, 8,3,5,  3,2,5,  3,7,2,
+        3,10,7, 10,6,7, 6,11,7, 6,0,11, 6,1,0,
+        10,1,6, 11,0,9, 2,11,9, 5,2,9,  11,2,7
+    };
+
+    meshData.Vertices.resize(12);
+    meshData.Indices32.assign(&k[0], &k[60]);
+
+    for (uint32 i = 0; i < 12; ++i)
+    {
+        meshData.Vertices[i].Position = pos[i];
+    }
+
+    for (uint32 i = 0; i < numSubdivisions; i++)
+    {
+        Subdivide(meshData);
+    }
+
+    // Project vertices on to sphere and scale.
+    for (uint32 i = 0; i < meshData.Vertices.size(); i++)
+    {
+        // Project onto unit sphere.
+        XMVECTOR n = XMVector3Normalize(XMLoadFloat3(&meshData.Vertices[i].Position));
+
+        // Project onto sphere.
+        XMVECTOR p = radius * n;
+    }
+
+}
