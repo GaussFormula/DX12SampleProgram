bf941b398d5282bb3a7418b97a522693fa796f3e
GaussFormula <c594794@hotmail.com>
2020-07-26 00:17:58 +0800

    Updated.

--- DX12SampleProgram/ShapesApp.cpp
+++ DX12SampleProgram/ShapesApp.cpp
@@ -47,6 +47,107 @@ void ShapesApp::BuildRootSignature()
     ));
 }
 
+void ShapesApp::BuildShadersAndInputLayout()
+{
+#if defined(_DEBUG)||(DEBUG)
+    // Enable better shader debugging with the graphics debugging tools.
+    UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#else
+    UINT compileFlags = 0;
+#endif
+    ThrowIfFailed(D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "VSMain", "vs_5_0", compileFlags, 0, &m_shaders["standardVS"], nullptr));
+    ThrowIfFailed(D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "PSMain", "ps_5_0", compileFlags, 0, &m_shaders["opaquePS"], nullptr));
+
+    m_inputLayout =
+    {
+        {"POSITION",0,DXGI_FORMAT_R32G32_FLOAT,0,0,D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0},
+        {"COLOR",0,DXGI_FORMAT_R32G32B32A32_FLOAT,0,12,D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0}
+    };
+}
+
+void ShapesApp::BuildShapesGeometry()
+{
+    GeometryGenerator geoGen;
+    GeometryGenerator::MeshData sphere = geoGen.CreateSphere(0.5f, 20, 20);
+    GeometryGenerator::MeshData pyramid = geoGen.CreatePyramid(20, 20, 0.5f, 3.0f);
+
+    // We are concatenating all the geometry into one big vertex/index buffer.
+    // So define the regions in the buffer each submesh covers.
+
+    // Cache the vertex offsets to each object in the concatenated vertex buffer.
+    UINT sphereVertexOffset = 0;
+    UINT pyramidVertexOffset = (UINT)sphere.Vertices.size();
+    
+    // Cache the starting index for each object in the concatenated index buffer.
+    UINT sphereIndexOffset = 0;
+    UINT pyramidIndexOffset = (UINT)sphere.Indices32.size();
+
+    // Define the SubmeshGeometry that cover different 
+    // regions of the vertex/index buffers.
+    SubmeshGeometry sphereSubmesh;
+    sphereSubmesh.IndexCount = (UINT)sphere.Indices32.size();
+    sphereSubmesh.StartIndexCount = sphereIndexOffset;
+    sphereSubmesh.BaseVertexLocation = sphereVertexOffset;
+
+    SubmeshGeometry pyramidSubmesh;
+    pyramidSubmesh.IndexCount = (UINT)pyramid.Indices32.size();
+    pyramidSubmesh.StartIndexCount = pyramidIndexOffset;
+    pyramidSubmesh.BaseVertexLocation = pyramidVertexOffset;
+
+    // Extract the vertex elements we are interested in and pack the 
+    // vertices of all the meshes into one vertex buffer.
+    auto totalVertexCount = sphere.Indices32.size() + pyramid.Indices32.size();
+
+    std::vector<Vertex> vertices(totalVertexCount);
+    UINT k = 0;
+    for (size_t i = 0; i < sphere.Vertices.size(); ++i, ++k)
+    {
+        vertices[k].Pos = sphere.Vertices[i].Position;
+        vertices[k].Color = XMFLOAT4(DirectX::Colors::DarkGreen);
+    }
+    for (size_t i=0;i<pyramid.Vertices.size();++i,++k)
+    {
+        vertices[k].Pos = pyramid.Vertices[i].Position;
+        vertices[k].Color = XMFLOAT4(DirectX::Colors::ForestGreen);
+    }
+
+    std::vector<std::uint16_t> indices;
+    indices.insert(indices.end(), std::begin(sphere.GetIndices16()), std::end(sphere.GetIndices16()));
+    indices.insert(indices.end(), std::begin(pyramid.GetIndices16()), std::end(pyramid.GetIndices16()));
+
+    const UINT vbByteSize = (UINT)vertices.size() * sizeof(Vertex);
+    const UINT ibByteSize = (UINT)indices.size() * sizeof(std::uint16_t);
+
+    auto geo = std::make_unique<MeshGeometry>();
+    geo->Name = "shapeGeo";
+
+    ThrowIfFailed(D3DCreateBlob(vbByteSize, &geo->VertexBufferCPU));
+    CopyMemory(geo->VertexBufferCPU->GetBufferPointer(), vertices.data(), vbByteSize);
+
+    ThrowIfFailed(D3DCreateBlob(ibByteSize, &geo->IndexBufferCPU));
+    CopyMemory(geo->IndexBufferCPU->GetBufferPointer(), indices.data(), ibByteSize);
+
+    geo->VertexBufferGPU = CreateDefaultBuffer(
+        m_device.Get(), m_commandList.Get(), vertices.data(),
+        vbByteSize, geo->VertexBufferUploader
+    );
+
+    geo->IndexBufferGPU = CreateDefaultBuffer(
+        m_device.Get(), m_commandList.Get(), indices.data(),
+        ibByteSize, geo->IndexBufferUploader
+    );
+
+    geo->VertexByteStride = sizeof(Vertex);
+    geo->VertexBufferByteSize = vbByteSize;
+    geo->IndexFormat = DXGI_FORMAT_R16_UINT;
+    geo->IndexBufferByteSize = ibByteSize;
+
+    geo->DrawArags["sphere"] = sphereSubmesh;
+    geo->DrawArags["pyramid"] = pyramidSubmesh;
+
+    m_geometries[geo->Name] = std::move(geo);
+}
+
 bool ShapesApp::Initialize()
 {
     if (!D3DAppBase::Initialize())
