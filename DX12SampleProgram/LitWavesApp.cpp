6a015f53a02a3ba8e8ca000faf62d7e1592d0479
GaussFormula <c594794@hotmail.com>
2020-08-21 18:44:30 +0800

    Updated.

--- DX12SampleProgram/LitWavesApp.cpp
+++ DX12SampleProgram/LitWavesApp.cpp
@@ -1,2 +1,12 @@
 #include "stdafx.h"
 #include "LitWavesApp.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+LitWavesApp::LitWavesApp(HINSTANCE hInstance)
+    :D3DAppBase(hInstance)
+{
+
+}
