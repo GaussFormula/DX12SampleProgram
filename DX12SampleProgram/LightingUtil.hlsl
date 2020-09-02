e37ef9d8362d53718a024d019c12c6cd3631db66
GaussFormula <c594794@hotmail.com>
2020-09-03 02:35:43 +0800

    Updated.

--- DX12SampleProgram/LightingUtil.hlsl
+++ DX12SampleProgram/LightingUtil.hlsl
@@ -34,7 +34,7 @@ float3 ShlickFresnel(float3 R0, float3 normal, float3 lightVec)
     float f0 = 1.0f - cosIncidentAngle;
     float3 reflectPercent = R0 + (1.0f - R0) * (f0 * f0 * f0 * f0 * f0);
 
-    return relectPercent;
+    return reflectPercent;
 }
 
 float3 BlinnPhong(float3 lightStrength, float3 lightVec, float3 normal, float3 toEye, Material mat)
@@ -43,5 +43,14 @@ float3 BlinnPhong(float3 lightStrength, float3 lightVec, float3 normal, float3 t
     float3 halfVec = normalize(toEye + lightVec);
 
     float roughnessFactor = (m + 8.0f) * pow(max(dot(halfVec, normal), 0.0f), m) / 8.0f;
-    float fresnelFactor = ShlickFresnel(mat.FresnelR0, halfVec, lightVec);
+    float3 fresnelFactor = ShlickFresnel(mat.FresnelR0, halfVec, lightVec);
+
+    float3 specularAlbedo = fresnelFactor * roughnessFactor;
+
+    // Our spec formula goes outside [0,1] range, but we are 
+    // doing LDR rendering. So scale it down a bit.
+    specularAlbedo = specularAlbedo/(specularAlbedo+1.0f);
+
+    return (mat.DiffuseAlbedo.rgb+specularAlbedo)*lightStrength;
 }
+
